44a4db7e2b6b00e8a4bb11af4764d614cc998839
masan4444 <masanori0213@gmail.com>
2019-06-05 16:32:36 +0000

    [add] shell script for RTIMULib2's installer

--- imu/RTIMULib2_installer.sh
+++ imu/RTIMULib2_installer.sh
@@ -3,15 +3,19 @@
 # これはMPU9250のセンサフュージョンを可能にするRTIMULib2のインストール用シェルスクリプトです．
 # これを実行したディレクトリ上にライブラリをクローンし，ライブラリをビルド・インストールします．
 
-sudo apt install cmake
-sudo apt install python-dev
+# まず下記を実行したください．
+# sudo apt install cmake
+# sudo apt install python-dev
 
-echo "--githubからRTIMULib2をクローンします---"
+echo "GithubからRTIMULib2をクローンします"
 git clone https://github.com/richards-tech/RTIMULib2.git
+
+echo "ライブラリをコンパイルします"
 cd RTIMULib2/Linux/RTIMULibCal
 make -j4
 sudo make install
 
+echo "pythonラッパーをインストールします"
 cd ../python
 python3 setup.py build
 sudo python3 setup.py install
