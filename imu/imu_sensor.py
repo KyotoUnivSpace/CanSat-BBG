824db67fc32bf6882627977a54aa34192c33e4f8
masan4444 <unixer.pro@h212.136.231.10.1017420.vlan.kuins.net>
2019-06-12 20:02:18 +0900

    [add] IMUWrapper Class

--- imu/imu_sensor.py
+++ imu/imu_sensor.py
@@ -1,4 +1,30 @@
+import sys
+import RTIMU
+import os.path
+import time
+import math
+
+SETTINGS_FILE = "RTIMULib"
+
 class IMUWrapper:
     def __init__(self):
-        pass
+        self.imu = RTIMU.RTIMU(RTIMU.Settings(SETTINGS_FILE))
+        # this is a good time to set any fusion parameters
+        self.imu.setSlerpPower(0.02)
+        self.imu.setGyroEnable(True)
+        self.imu.setAccelEnable(True)
+        self.imu.setCompassEnable(True)
+    def getGyro(self):
+        if self.imu.IMURead():
+            return self.imu.getGyro();
+    def getFusionPose(self):
+        if self.imu.IMURead():
+            return self.imu.getFusionData()
 
+if __name__ == '__main__':
+    imu = IMUWrapper();
+    poll_interval = imu.IMUGetPollInterval()
+    while True:
+        r, p, y = imu.getFusionPose();
+        print("pose[r]: %f pose[p]: %f pose[y]: %f" % (math.degrees(r), math.degrees(p), math.degrees(y)))
+        time.sleep(poll_interval*1.0/1000.0)
