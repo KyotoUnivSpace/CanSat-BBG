45ab10d2b3d11fe42dafda6d212d1e9dbdfaaee0
masan4444 <unixer.pro@h87.132.231.10.1017420.vlan.kuins.net>
2019-06-19 20:04:45 +0900

    [fix] IMUWrapper Class

--- imu/imu_sensor.py
+++ imu/imu_sensor.py
@@ -3,28 +3,44 @@ import RTIMU
 import os.path
 import time
 import math
+sys.path.append('.')
 
 SETTINGS_FILE = "RTIMULib"
 
 class IMUWrapper:
     def __init__(self):
-        self.imu = RTIMU.RTIMU(RTIMU.Settings(SETTINGS_FILE))
+        print("Using settings file " + SETTINGS_FILE + ".ini")
+        if not os.path.exists(SETTINGS_FILE + ".ini"):
+            print("Settings file does not exist, will be created")
+        self.s = RTIMU.Settings(SETTINGS_FILE)
+        self.imu = RTIMU.RTIMU(self.s)
+        print("IMU Name: " + self.imu.IMUName())
+        # print(self.imu.IMUInit())
+        if not self.imu.IMUInit():
+            print("IMU Init Failed")
+            sys.exit(1)
+        else:
+            print("IMU Init Succeeded")
         # this is a good time to set any fusion parameters
         self.imu.setSlerpPower(0.02)
         self.imu.setGyroEnable(True)
         self.imu.setAccelEnable(True)
         self.imu.setCompassEnable(True)
     def getGyro(self):
-        if self.imu.IMURead():
-            return self.imu.getGyro();
+        return self.imu.getGyro();
     def getFusionPose(self):
-        if self.imu.IMURead():
-            return self.imu.getFusionData()
+        return self.imu.getFusionData()
+    def read(self):
+        return self.imu.IMURead()
+    def getPollInterval(self):
+        return self.imu.IMUGetPollInterval()
 
 if __name__ == '__main__':
-    imu = IMUWrapper();
-    poll_interval = imu.IMUGetPollInterval()
+    imuWrapper = IMUWrapper();
+    poll_interval = imuWrapper.getPollInterval()
     while True:
-        r, p, y = imu.getFusionPose();
-        print("pose[r]: %f pose[p]: %f pose[y]: %f" % (math.degrees(r), math.degrees(p), math.degrees(y)))
-        time.sleep(poll_interval*1.0/1000.0)
+        if imuWrapper.read():
+            (r, p, y) = imuWrapper.getFusionPose();
+            (_r, _p, _y) = imuWrapper.getGyro();
+            print("pose[r]:%f pose[p]:%f pose[y]:%f   gyro[r]:%f gyro[p]:%f gyro[y]:%f" % (math.degrees(r), math.degrees(p), math.degrees(y), math.degrees(_r), math.degrees(_p), math.degrees(_y)))
+            time.sleep(poll_interval*1.0/1000.0)
