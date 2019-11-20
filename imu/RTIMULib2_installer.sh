73e1c9e4b960e53ee666045eab6b75009058f847
komamiku <mikuma1503@gmail.com>
2019-11-20 19:59:43 +0900

    Add color echo to imu installer

--- imu/RTIMULib2_installer.sh
+++ imu/RTIMULib2_installer.sh
@@ -3,21 +3,25 @@
 # これはMPU9250のセンサフュージョンを可能にするRTIMULib2のインストール用シェルスクリプトです．
 # これを実行したディレクトリ上にライブラリをクローンし，ライブラリをビルド・インストールします．
 
+emecho() {
+    echo "\e[1;35m$1\e[m"
+}
+
 cd $(dirname $0)
 
-echo "環境構築中..."
+emecho "環境構築中..."
 sudo apt install cmake
 sudo apt install python-dev
 
-echo "GithubからRTIMULib2をクローンします"
+emecho "GithubからRTIMULib2をクローンします"
 git clone https://github.com/richards-tech/RTIMULib2.git
 
-echo "ライブラリをコンパイルします"
+emecho "ライブラリをコンパイルします"
 cd RTIMULib2/Linux/RTIMULibCal
 make -j4
 sudo make install
 
-echo "pythonラッパーをインストールします"
+emecho "pythonラッパーをインストールします"
 cd ../python
 python3 setup.py build
 sudo python3 setup.py install
